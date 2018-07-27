0aeb24d8a445329d428ff7056166e97a46f875f7
Berinjah <lucas.silva2808@etec.sp.gov.br>
2018-07-27 20:47:46 -0300

    Model, View, Controller em Java

---
+++ Exemplo_Banco_de_Dados/src/view/AlunosExec.java
@@ -0,0 +1,28 @@
+package view;
+
+import java.sql.Connection;
+
+import controller.AlunosJdbcDAO;
+import controller.JdbUtil;
+import model.Alunos;
+
+public class AlunosExec {
+
+	public static void main(String[] args) {
+		Alunos alunos = new Alunos();
+		try {
+			alunos.setNome("Lucas Cardoso");
+			alunos.setEndereco("Av Águia de Haia, 2600");
+			alunos.setBairro("Jd São Nicolau");
+			alunos.setCep(3811111);
+			
+			Connection connection = JdbUtil.getConnection();
+			AlunosJdbcDAO alunosJdbcDao = new AlunosJdbcDAO(connection);
+			
+			alunosJdbcDao.salvar(alunos);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
